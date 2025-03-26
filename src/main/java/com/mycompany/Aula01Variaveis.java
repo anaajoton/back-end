acb9acba7257720f54e8ff624c702102ffa7bcb9
anaajoton <ana.joton@escola.pr.gov.br>
2025-03-26 13:51:23 +0000

    aula01devariaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+    public static void main(String[] args) {
+        // Variavel lógica
+
+        // Tipo primitivo
+        boolean valorLógicoPrimitivo = true;
+
+        // Tipo abstrato
+        Boolean valorlogicoabstrato = false;01
+    }
+}
