e42d43cdbb066b43b9934811771f0d375313abc4
anaajoton <ana.joton@escola.pr.gov.br>
2025-03-28 13:43:11 +0000

    aula 2 sobre variaveis

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -1,13 +1,46 @@
-package com.mycompany;
+package com.my.company;
 
 public class Aula01Variaveis {
+
     public static void main(String[] args) {
-        // Variavel lógica
+        // Variavel lógica 
+
+        //Tipo primitivo
+
+        boolean ValorLogicoPrimitivo = true;
+        //exibe o valor
+        System.out.println(valorLogicoPrimitivo);
+
+        //Tipo abstrato
+        Boolean valorLogicoAbstrato = false;
+        // Exibe o texto
+        System.out.println("O valor da variavel valorLogicoAbstrato é: " + valorLogicoPrimitivo);
+        
+        // Variáveis inteiras 
 
         // Tipo primitivo
-        boolean valorLógicoPrimitivo = true;
+        int valorInteiro = 10; //32bits
+        long valorInteiroLongo = 10L; // 64bits
 
-        // Tipo abstrato
-        Boolean valorlogicoabstrato = false;01
+        // Tipo abstrato 
+        Integer valorInteiroAbs = 20;
+        Long valorLongo = 20L; 
+
+        System.out,println("O valor de valorInteiro é: " + valorInteiro + ", e o valor de 
+        valorInteiroLongo é : " + valorInteiro);
+
+        // Variáveis Decimais 
+
+        // Tipo Primitivo
+        float numeroFloat = 10.5f; // 32 =bits
+        double numeroDoibleAbs = 10.5; //64bits
+
+        //Tipo abstrato
+        Float numeroFloat = 10.5f; 
+        Double numeroDoibleAbs = 10.5;
+
+        Sytem.out.println("O valor de numeroFloat é: " + numeroFloat + ", e o valor de numeroDouble é: "
+        + numeroDouble);
+
     }
 }
