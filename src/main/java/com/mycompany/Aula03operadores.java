74ea24ac2cc15b925d4965efb3498fc76a8e8cc2
anaajoton <ana.joton@escola.pr.gov.br>
2025-04-02 19:40:44 +0000

    Aula03operadores.java

---
+++ src/main/java/com/mycompany/Aula03operadores.java
@@ -0,0 +1,16 @@
+package com.mycompany;
+
+public class Aula03operadores {
+
+public static void main(String[] args) {
+    /** */
+     * Tipos de Comentários
+     * /
+
+}    // Comentário de uma Linha
+
+     /** 
+      *  Comentário de
+      * mùltiplas
+      * linhas
+      */
