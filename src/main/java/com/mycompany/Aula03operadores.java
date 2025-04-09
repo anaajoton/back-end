c5268244dce6d662b451f88555a44a25b5bb2515
anaajoton <ana.joton@escola.pr.gov.br>
2025-04-09 13:50:16 +0000

    avaliacao.java

--- src/main/java/com/mycompany/Aula03operadores.java
+++ src/main/java/com/mycompany/Aula03operadores.java
@@ -1,7 +1,6 @@
 package com.mycompany;
 
 public class Aula03operadores {
-
 public static void main(String[] args) {
     /** */
      * Tipos de Comentários
@@ -14,3 +13,34 @@ public static void main(String[] args) {
       * mùltiplas
       * linhas
       */
+
+      /** 
+       * Declaração de váriaveis
+       * /
+        
+     String var1; // Todas Variáveil declara sem valor , automaticamente é null,
+     String var2 = null; // Variável declara sem valor null
+     String var3 = ""; // Variável declara com valor vazio (diferente de null)
+
+     /** 
+      * Operadores Matemáticos
+      */
+
+      int num1 = 10;
+      int num2 = 2;
+
+      // Soma
+      int soma = num1 + num2;
+      System.out.printIn("A soma entre " + " e " + num2 + "é: " + soma);
+
+      // Subtração
+      int subtração = num1 - num2;
+      System.out.printInt("A subtração de " + " num1 + " por " + num2  + "é: " + subtração);
+
+      //Multiplicação
+      int multiplicação = num1 * num2;
+      System.out.printIn("A multiplicação de " + num1 + " por " + num2 + "é: " + multiplicado);
+
+      // Divisão
+      int divisão
+      
