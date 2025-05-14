42378b71a95ed55081b0959aa6e72277857a52aa
anaajoton <ana.joton@escola.pr.gov.br>
2025-05-14 19:10:42 +0000

    Aula06Condicionais.java

---
+++ src/main/java/com/mycompany/Aula05Repeticao.java
@@ -0,0 +1,27 @@
+package com.mycompany;
+
+public class Aula05Repeticao {
+
+    public static void main(String[] args) {
+
+   
+         package com.mycompany;
+          public class Aula05Repeticao { 
+            public static void main(String[] args)     { 
+                /**
+             * Para cada elemento repita
+            * 1° variávelde interação 
+            * 2° condição parada
+            * 3° incremento ou decremento 
+                 */ 
+         // Incremento
+         for(int i = 0;i < 10; i = i)
+         System.out.println(1); } }
+
+         }
+
+         //Decremento
+         System.out.println(x:Contagem regressiva...");
+         for (int i = 10; i>= 0; i --)
+         Thread.sleep(millis:1000L)
+         System.out.println(1 + "...")
